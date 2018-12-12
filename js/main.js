f4b79904ad10f4e85ee1cada116282201a53c404
kcarri <44794979+kcarri@users.noreply.github.com>
2018-12-11 23:08:03 -0600

    rework

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('#btn-animate').on('click',function() {
+        $('#hinged').addClass('animated hinge')
+        $('#hinged2').addClass('animated hinge delay-2s')
+        $('#hinged3').addClass('animated hinge delay-3s')
+    })
+
+
+    setTimeout(function() {
+        $('#rockedout').fadeOut(200);
+    }, 900);
+
+});
+
